7b564bf0534f1a998e626fdc7f7e7a8aea053c2d
sasquatch13 <115385918+sasquatch13@users.noreply.github.com>
2023-01-29 18:18:17 -0500

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let newNum = 0;
+  let x = num1;
+  let y = num2;
+  while (num1 != num2) {
+  num1 + newNum;
+  num1++
+}
 };
 
 // Do not edit below this line
